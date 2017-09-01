f004e45b237200ec7e2b9e43747b4ccccfe8b8f2
Jonathan Velazquez <israel.velazquez@globant.com>
2017-08-31 21:10:55 -0500

    Configuration for ProductViewCell

--- Linio/ProductViewCell.swift
+++ Linio/ProductViewCell.swift
@@ -9,14 +9,37 @@
 import UIKit
 
 class ProductViewCell: UICollectionViewCell {
+    
     @IBOutlet weak var productImage: UIImageView!
-    @IBOutlet weak var linioPlusIcon: UIImageView!
-    @IBOutlet weak var conditionTypeIcon: UIImageView!
-    @IBOutlet weak var importedIcon: UIImageView!
-    @IBOutlet weak var shippingIcon: UIImageView!
+    @IBOutlet var iconsImages: [UIImageView]!
+    @IBOutlet weak var stackView: UIStackView!
+    @IBOutlet weak var likeIcon: UIImageView!
+    
     
     override func awakeFromNib() {
     }
     
+    func configCell(withProduct product:ProductProtocol) {
+        
+        NetworkHelper.downloadImage(url: product.imageURL, imageView: productImage)
+        
+        var iconsAvailables = [UIImage]()
+        if let plusLevel = product.linioPlusLevel {iconsAvailables.append(plusLevel)}
+        if let condition = product.condition { iconsAvailables.append(condition)}
+        if let imported = product.imported { iconsAvailables.append(imported)}
+        if let freeShipping = product.freeShipping { iconsAvailables.append(freeShipping)}
+        
+        for i in 0...(iconsImages.count-1) {
+            if i < iconsAvailables.count {
+                iconsImages[i].image = iconsAvailables[i]
+            }else{
+                iconsImages[i].image = UIImage()
+            }
+        }
+        
+        self.contentView.bringSubview(toFront: stackView)
+        self.contentView.bringSubview(toFront: likeIcon)
+
+    }
     
 }
